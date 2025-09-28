17c505f85934a5f8be9fdd4413779f4a80921f96
Carlos A M Ferreira <c.a.mascarenhas@hotmail.com>
2025-09-28 09:56:12 -0300

    feat: add documentation for PublishVideoStatusUseCase interface and publishStatus method

--- src/main/java/br/com/video/splitter/application/usecase/PublishVideoStatusUseCase.java
+++ src/main/java/br/com/video/splitter/application/usecase/PublishVideoStatusUseCase.java
@@ -2,6 +2,16 @@ package br.com.video.splitter.application.usecase;
 
 import java.util.UUID;
 
+/**
+ * Interface use case para publicar o status do vídeo.
+ */
 public interface PublishVideoStatusUseCase {
+    /**
+     * Publica o status do vídeo.
+     *
+     * @param userId  ID do usuário.
+     * @param videoId ID do vídeo.
+     * @param status  Status do vídeo.
+     */
     void publishStatus(UUID userId, Long videoId, String status);
 }
